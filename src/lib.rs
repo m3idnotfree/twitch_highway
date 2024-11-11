aa9835dd88ed559ada184eaa94571d9f8f69d7f2
m3id <m3id.eprac@gmail.com>
2024-11-12 04:04:58 +0900

    Feat: features chat, eventsub, users, full

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
+#[cfg(feature = "chat")]
 mod chat;
+#[cfg(feature = "chat")]
 pub use chat::*;
+
+#[cfg(feature = "eventsub")]
+mod eventsub;
+#[cfg(feature = "eventsub")]
+pub use eventsub::*;
+
+#[cfg(feature = "users")]
+mod users;
+#[cfg(feature = "users")]
+pub use users::*;
+
 mod error;
 pub use error::*;
-pub mod eventsub;
-pub mod users;
 
 pub type Result<TR> = std::result::Result<TR, crate::Error>;
