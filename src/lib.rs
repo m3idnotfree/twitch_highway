36f87baa5b57e6b90bd3b001a6b521d7aeca233c
m3id <m3id.eprac@gmail.com>
2024-12-18 16:54:02 +0900

    docs: update API endpoints status and roadmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,63 @@
 //! implement Endpoint
 //!
 //! Users
-//! - get users
+//!  [x] Get Users
+//!  [ ] Update User
+//!  [ ] Get User Block List
+//!  [ ] Block User
+//!  [ ] Unblock User
+//!  [ ] Get User Extensions
+//!  [ ] Get User Active Extensions
+//!  [ ] Update User Extensions
 //!
 //! EventSubAPI
-//! - create
-//! - delete
+//!  [x] Create EventSub Subscription
+//!  [x] Delete EventSub Subscription
+//!  [ ] Get EventSub Subscriptions
 //!
 //! Chat
-//! - emotes
-//!  - channel
-//!  - global
-//!  - emote sets
-//! - badges
-//!  - channel
-//!  - global
-//! - chatters
+//!  [x] Get Channel Emotes
+//!  [x] Get Global Emotes
+//!  [x] Get Emote Sets
+//!  [x] Get Channel Chat Badges
+//!  [x] Get Global Chat Badges
+//!  [x] Get Chat Settings
+//!  [x] Get Shared Chat Session
+//!  [ ] Get User Emotes
+//!  [ ] Update Chat Settings
+//!  [ ] Send Chat Announcement
+//!  [ ] Send a Shoutout
+//!  [ ] Send Chat Message
+//!  [ ] Get User Chat Color
+//!  [ ] Update User Chat Color
+//!
+//! Todo
+//! Ads
+//! Bits
+//! Channels
+//! Channel Points
+//! Charity
+//! Clips
+//! Conduits
+//! CCLs
+//! Entitlements
+//! Extensions
+//! Games
+//! Goals
+//! Guest Star
+//! Hype Train
+//! Moderation
+//! Polls
+//! Predictions
+//! Raids
+//! Schedule
+//! Search
+//! Streams
+//! Subscriptions
+//! Tags
+//! Teams
+//! Videos
+//! Whispers
 
 pub(crate) const TWITCH_API_BASE: &str = "https://api.twitch.tv/helix";
 #[cfg(feature = "chat")]
