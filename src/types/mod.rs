30d8ffcb7ef43e72f9972f10ccd0f2d638679c64
m3id <m3id.eprac@gmail.com>
2024-12-24 04:15:15 +0900

    feat(eventsub): SubscriptionTypes support deserialization

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,3 +2,5 @@ mod pagination;
 pub use pagination::*;
 mod subscription_types;
 pub use subscription_types::*;
+mod transport;
+pub use transport::*;
--- src/types/subscription_types.rs
+++ src/types/subscription_types.rs
@@ -1,5 +1,6 @@
-use serde::{Serialize, Serializer};
+use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
 
+/// https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SubscriptionTypes {
     // Automod related
@@ -237,5 +238,109 @@ impl Serialize for SubscriptionTypes {
         S: Serializer,
     {
         serializer.serialize_str(self.as_str())
+    }
+}
+
+impl<'de> Deserialize<'de> for SubscriptionTypes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        match s.as_str() {
+            "automod.message.hold" => Ok(Self::AutomodMessageHold),
+            // "automod.message.hold" => Ok(Self::AutomodMessageHoldV2),
+            "automod.message.update" => Ok(Self::AutomodMessageUpdate),
+            // "automod.message.update" => Ok(Self::AutomodMessageUpdateV2),
+            "automod.settings.update" => Ok(Self::AutomodSettingsUpdate),
+            "automod.terms.update" => Ok(Self::AutomodTermsUpdate),
+            "channel.update" => Ok(Self::ChannelUpdate),
+            "channel.follow" => Ok(Self::ChannelFollow),
+            "channel.ad_break.begin" => Ok(Self::ChannelAdBreakBegin),
+            "channel.chat.clear" => Ok(Self::ChannelChatClear),
+            "channel.chat.clear_user_messages" => Ok(Self::ChannelChatClearUserMessages),
+            "channel.chat.message" => Ok(Self::ChannelChatMessage),
+            "channel.chat.message_delete" => Ok(Self::ChannelChatMessageDelete),
+            "channel.chat.notification" => Ok(Self::ChannelChatNotification),
+            "channel.chat_settings.update" => Ok(Self::ChannelChatSettingsUpdate),
+            "channel.chat.user_message_hold" => Ok(Self::ChannelChatUserMessageHold),
+            "channel.chat.user_message_update" => Ok(Self::ChannelChatUserMessageUpdate),
+            "channel.shared_chat.begin" => Ok(Self::ChannelSharedChatSessionBegin),
+            "channel.shared_chat.update" => Ok(Self::ChannelSharedChatSessionUpdate),
+            "channel.shared_chat.end" => Ok(Self::ChannelSharedChatSessionEnd),
+            "channel.subscribe" => Ok(Self::ChannelSubscribe),
+            "channel.subscription.end" => Ok(Self::ChannelSubscriptionEnd),
+            "channel.subscription.gift" => Ok(Self::ChannelSubscriptionGift),
+            "channel.subscription.message" => Ok(Self::ChannelSubscriptionMessage),
+            "channel.cheer" => Ok(Self::ChannelCheer),
+            "channel.raid" => Ok(Self::ChannelRaid),
+            "channel.ban" => Ok(Self::ChannelBan),
+            "channel.unban" => Ok(Self::ChannelUnban),
+            "channel.unban_request.create" => Ok(Self::ChannelUnbanRequestCreate),
+            "channel.unban_request.resolve" => Ok(Self::ChannelUnbanRequestResolve),
+            "channel.moderate" => Ok(Self::ChannelModerate),
+            "channel.moderator.add" => Ok(Self::ChannelModeratorAdd),
+            "channel.moderator.remove" => Ok(Self::ChannelModeratorRemove),
+            "channel.guest_star_session.begin" => Ok(Self::ChannelGuestStarSessionBegin),
+            "channel.guest_star_session.end" => Ok(Self::ChannelGuestStarSessionEnd),
+            "channel.guest_star_guest.update" => Ok(Self::ChannelGuestStarGuestUpdate),
+            "channel.guest_star_settings.update" => Ok(Self::ChannelGuestStarSettingsUpdate),
+            "channel.channel_points_automatic_reward_redemption.add" => {
+                Ok(Self::ChannelPointsAutomaticRewardRedemption)
+            }
+            "channel.channel_points_custom_reward.add" => Ok(Self::ChannelPointsCustomRewardAdd),
+            "channel.channel_points_custom_reward.update" => {
+                Ok(Self::ChannelPointsCustomRewardUpdate)
+            }
+            "channel.channel_points_custom_reward.remove" => {
+                Ok(Self::ChannelPointsCustomRewardRemove)
+            }
+            "channel.channel_points_custom_reward_redemption.add" => {
+                Ok(Self::ChannelPointsCustomRewardRedemptionAdd)
+            }
+            "channel.channel_points_custom_reward_redemption.update" => {
+                Ok(Self::ChannelPointsCustomRewardRedemptionUpdate)
+            }
+            "channel.poll.begin" => Ok(Self::ChannelPollBegin),
+            "channel.poll.progress" => Ok(Self::ChannelPollProgress),
+            "channel.poll.end" => Ok(Self::ChannelPollEnd),
+            "channel.prediction.begin" => Ok(Self::ChannelPredictionBegin),
+            "channel.prediction.progress" => Ok(Self::ChannelPredictionProgress),
+            "channel.prediction.lock" => Ok(Self::ChannelPredictionLock),
+            "channel.prediction.end" => Ok(Self::ChannelPredictionEnd),
+            "channel.suspicious_user.message" => Ok(Self::ChannelSuspiciousUserMessage),
+            "channel.suspicious_user.update" => Ok(Self::ChannelSuspiciousUserUpdate),
+            "channel.vip.add" => Ok(Self::ChannelVIPAdd),
+            "channel.vip.remove" => Ok(Self::ChannelVIPRemove),
+            "channel.warning.acknowledge" => Ok(Self::ChannelWarningAcknowledgement),
+            "channel.warning.send" => Ok(Self::ChannelWarningSend),
+            "channel.charity_campaign.donate" => Ok(Self::CharityDonation),
+            "channel.charity_campaign.start" => Ok(Self::CharityCampaignStart),
+            "channel.charity_campaign.progress" => Ok(Self::CharityCampaignProgress),
+            "channel.charity_campaign.stop" => Ok(Self::CharityCampaignStop),
+            "conduit.shard.disabled" => Ok(Self::ConduitShardDisabled),
+            "drop.entitlement.grant" => Ok(Self::DropEntitlementGrant),
+            "extension.bits_transaction.create" => Ok(Self::ExtensionBitsTransactionCreate),
+            "channel.goal.begin" => Ok(Self::GoalBegin),
+            "channel.goal.progress" => Ok(Self::GoalProgress),
+            "channel.goal.end" => Ok(Self::GoalEnd),
+            "channel.hype_train.begin" => Ok(Self::HypeTrainBegin),
+            "channel.hype_train.progress" => Ok(Self::HypeTrainProgress),
+            "channel.hype_train.end" => Ok(Self::HypeTrainEnd),
+            "channel.shield_mode.begin" => Ok(Self::ShieldModeBegin),
+            "channel.shield_mode.end" => Ok(Self::ShieldModeEnd),
+            "channel.shoutout.create" => Ok(Self::ShoutoutCreate),
+            "channel.shoutout.receive" => Ok(Self::ShoutoutReceived),
+            "stream.online" => Ok(Self::StreamOnline),
+            "stream.offline" => Ok(Self::StreamOffline),
+            "user.authorization.grant" => Ok(Self::UserAuthorizationGrant),
+            "user.authorization.revoke" => Ok(Self::UserAuthorizationRevoke),
+            "user.update" => Ok(Self::UserUpdate),
+            "user.whisper.message" => Ok(Self::WhisperReceived),
+            _ => Err(D::Error::custom(format!(
+                "unknown subscription type: '{}'",
+                s
+            ))),
+        }
     }
 }
