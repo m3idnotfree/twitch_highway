16a0b3924f9d95585f21ee0e12f09864c2bc6253
m3id <m3id.eprac@gmail.com>
2025-01-11 14:59:26 +0900

    feat(error): update

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,19 @@
+use asknothingx2_util::api;
+
+use crate::request::APIError;
+
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("URL parse error {0}")]
     UrlParseError(#[from] url::ParseError),
     #[error("Missing Test URL")]
     MissingTestUrl,
+    #[error("Failed to deserialize response: {0}")]
+    DeserializationError(String),
+    #[error("API request failed: {0}")]
+    APIRequestFailed(#[from] api::ReqwestError),
+    #[error("Failed to deserialize successful response: {0}")]
+    ResponseDeserializeError(#[from] serde_json::Error),
+    #[error("API returned error response: {0}")]
+    TwitchAPIError(APIError),
 }
