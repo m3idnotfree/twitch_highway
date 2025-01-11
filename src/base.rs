2839ac22fddd2befa0f4c465b622c47f1d854850
m3id <m3id.eprac@gmail.com>
2025-01-11 15:06:30 +0900

    feat(base): add IntoQueryPairs trait

--- src/base.rs
+++ src/base.rs
@@ -4,14 +4,16 @@ use asknothingx2_util::{
 };
 use url::Url;
 
+const TWITCH_API_BASE: &str = "https://api.twitch.tv/helix";
+
 pub trait TwitchAPIBase {
     fn access_token(&self) -> &AccessToken;
     fn client_id(&self) -> &ClientId;
-    fn build_headers(&self) -> HH {
-        HH::base(self.access_token(), self.client_id())
+    fn build_headers(&self) -> HeadersBuilder {
+        HeadersBuilder::base(self.access_token(), self.client_id())
     }
-    fn build_url(&self) -> UU {
-        UU::new()
+    fn build_url(&self) -> UrlBuilder {
+        UrlBuilder::new()
     }
 }
 
@@ -39,18 +41,19 @@ impl TwitchAPIBase for TwitchAPI {
     }
 }
 
-pub struct HH(HeaderBuilder);
-impl HH {
+pub struct HeadersBuilder(HeaderBuilder);
+
+impl HeadersBuilder {
     pub fn base(access_token: &AccessToken, client_id: &ClientId) -> Self {
-        Self(
-            HeaderBuilder::new()
-                .authorization("Bearer", access_token.secret().as_str())
-                .client_id(client_id.as_str()),
-        )
+        let mut headers = HeaderBuilder::new();
+        headers
+            .authorization("Bearer", access_token.secret().as_str())
+            .client_id(client_id.as_str());
+        Self(headers)
     }
 
-    pub fn json(mut self) -> Self {
-        self.0 = self.0.content_type_json();
+    pub fn json(&mut self) -> &mut Self {
+        self.0.content_type_json();
         self
     }
 
@@ -59,50 +62,94 @@ impl HH {
     }
 }
 
-pub struct UU(Url);
+pub struct UrlBuilder(Url);
 
-impl Default for UU {
+impl Default for UrlBuilder {
     fn default() -> Self {
-        UU(url::Url::parse(crate::TWITCH_API_BASE).unwrap())
+        UrlBuilder(url::Url::parse(TWITCH_API_BASE).unwrap())
     }
 }
 
-impl UU {
+impl UrlBuilder {
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn path<T: AsRef<str>, L: IntoIterator<Item = T>>(mut self, path: L) -> Self {
+    pub fn path<T: AsRef<str>, L: IntoIterator<Item = T>>(&mut self, path: L) -> &mut Self {
         self.0.path_segments_mut().unwrap().extend(path);
         self
     }
 
     pub fn query<K: AsRef<str>, V: AsRef<str>, L: IntoIterator<Item = (K, V)>>(
-        mut self,
+        &mut self,
         querys: L,
-    ) -> Self {
+    ) -> &mut Self {
         self.0.query_pairs_mut().extend_pairs(querys);
         self
     }
 
-    pub fn query_option<T: AsRef<str>>(mut self, key: &str, value: Option<T>) -> Self {
+    pub fn query_opt<T: AsRef<str>>(&mut self, key: &str, value: Option<T>) -> &mut Self {
         if let Some(value) = value {
             self.0.query_pairs_mut().append_pair(key, value.as_ref());
         }
         self
     }
 
-    pub fn query_option_extend<K: AsRef<str>, V: AsRef<str>, L: IntoIterator<Item = (K, V)>>(
-        mut self,
+    pub fn query_opt_extend<K: AsRef<str>, V: AsRef<str>, L: IntoIterator<Item = (K, V)>>(
+        &mut self,
         querys: Option<L>,
-    ) -> Self {
+    ) -> &mut Self {
         if let Some(querys) = querys {
             self.0.query_pairs_mut().extend_pairs(querys);
         }
         self
     }
 
+    pub fn query_pairs<T: IntoQueryPairs>(&mut self, querys: T) -> &mut Self {
+        self.0
+            .query_pairs_mut()
+            .extend_pairs(querys.into_query_pairs());
+        self
+    }
+
     pub fn build(self) -> Url {
+        self.0
+    }
+}
+
+pub trait IntoQueryPairs {
+    fn into_query_pairs(self) -> Vec<(&'static str, String)>;
+}
+#[derive(Debug, Default)]
+pub struct QueryParams(Vec<(&'static str, String)>);
+
+impl QueryParams {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn push<T: Into<String>>(&mut self, key: &'static str, value: T) -> &mut Self {
+        self.0.push((key, value.into()));
+        self
+    }
+
+    pub fn push_opt<T: Into<String>>(&mut self, key: &'static str, value: Option<T>) -> &mut Self {
+        if let Some(v) = value {
+            self.0.push((key, v.into()));
+        }
+        self
+    }
+
+    pub fn extend<V: Into<String>, L: IntoIterator<Item = (&'static str, V)>>(
+        &mut self,
+        extend: L,
+    ) -> &mut Self {
+        self.0
+            .extend(extend.into_iter().map(|(x, y)| (x, y.into())));
+        self
+    }
+
+    pub fn build(self) -> Vec<(&'static str, String)> {
         self.0
     }
 }
