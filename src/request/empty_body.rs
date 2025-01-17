9615798433b2e2c632ec9deadfcfd75a3ce2e334
m3id <m3id.eprac@gmail.com>
2025-01-17 13:29:39 +0900

    refactor(request): reorganize requ module

---
+++ src/request/empty_body.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+use super::IntoRequestBody;
+
+#[derive(Debug, Serialize)]
+pub struct EmptyBody;
+impl IntoRequestBody for EmptyBody {}
+impl<'de> Deserialize<'de> for EmptyBody {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct EmptyVisitor;
+        impl serde::de::Visitor<'_> for EmptyVisitor {
+            type Value = EmptyBody;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("empty body")
+            }
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                if v.is_empty() {
+                    Ok(EmptyBody)
+                } else {
+                    Err(E::custom("expected empty string"))
+                }
+            }
+            fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_str(&v)
+            }
+        }
+        deserializer.deserialize_str(EmptyVisitor)
+    }
+}
---
+++ src/request/request_body.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+
+use crate::IntoRequestBody;
+
+#[derive(Serialize, Deserialize)]
+pub struct RequestBody<T, K> {
+    #[serde(flatten)]
+    required: T,
+    #[serde(flatten, skip_serializing_if = "Option::is_none")]
+    optional: Option<K>,
+}
+
+impl<T, K> RequestBody<T, K>
+where
+    T: Serialize,
+    K: Serialize,
+{
+    pub fn new(required: T, opts: Option<K>) -> Self {
+        Self {
+            required,
+            optional: opts,
+        }
+    }
+}
+
+impl<T, K> IntoRequestBody for RequestBody<T, K>
+where
+    T: Serialize,
+    K: Serialize,
+{
+    fn as_body(&self) -> Option<String> {
+        Some(serde_json::to_string(&self).unwrap())
+    }
+}
