6960083731322741b51949faa614e6bc82e33def
m3id <m3id.eprac@gmail.com>
2025-08-13 16:49:42 +0900

    test(cost): add serde roundtrip test

--- src/types/cost.rs
+++ src/types/cost.rs
@@ -18,3 +18,29 @@ impl Cost {
 pub enum CostType {
     Bits,
 }
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use crate::types::{Cost, CostType};
+
+    #[test]
+    fn rountrip_serde() {
+        let cost = Cost::new(150, CostType::Bits);
+
+        let serialized = serde_json::to_string(&cost).unwrap();
+        let expected = json!({
+            "amount": 150,
+            "type": "bits"
+        });
+
+        assert_eq!(
+            serde_json::from_str::<serde_json::Value>(&serialized).unwrap(),
+            expected
+        );
+
+        let deserialized: Cost = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(deserialized.amount, 150);
+    }
+}
