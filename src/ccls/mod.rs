dd31cf46db111c04c3b8660a3fce9fab72027376
m3id <m3id.eprac@gmail.com>
2025-10-11 11:33:39 +0900

    docs(ccls): document API endpoints

--- src/ccls/mod.rs
+++ src/ccls/mod.rs
@@ -8,6 +8,38 @@ pub use types::Ccl;
 
 endpoints! {
     CclsAPI {
+        /// Gets information about Twitch content classification labels
+        ///
+        /// # Arguments
+        ///
+        /// * `locale` - Optional locale for the Content Classification Labels.
+        ///
+        /// # Returns
+        ///
+        /// Returns a [`CclsResponse`]
+        ///
+        /// # Example
+        ///
+        /// ```rust
+        /// # use twitch_highway::TwitchAPI;
+        /// use twitch_highway::ccls::{CclLocale, CclsAPI};
+        ///
+        /// # async fn example(api: TwitchAPI) -> Result<(), Box<dyn std::error::Error>> {
+        /// let response = api
+        ///     .get_content_classification_labels(Some(CclLocale::deDE))
+        ///     .json()
+        ///     .await?;
+        ///
+        /// # Ok(())
+        /// # }
+        /// ```
+        ///
+        /// # Required Scope
+        ///
+        /// No scope required
+        ///
+        /// API Reference
+        ///
         /// <https://dev.twitch.tv/docs/api/reference/#get-content-classification-labels>
         fn get_content_classification_labels(
             &self,
