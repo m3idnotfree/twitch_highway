9cd8bb6dd8c1b354c3e3a360a5e43e2d2dc1b5d6
m3id <m3id.eprac@gmail.com>
2025-10-14 12:27:28 +0900

    refactor(goals): replace endpoints with simple_endpoint macro

--- src/goals/mod.rs
+++ src/goals/mod.rs
@@ -4,59 +4,62 @@ mod types;
 pub use response::GoalsResponse;
 pub use types::{Goal, GoalType};
 
-use crate::types::{
-    constants::{BROADCASTER_ID, GOALS},
-    BroadcasterId,
+use crate::{
+    request::TwitchAPIRequest,
+    types::{
+        constants::{BROADCASTER_ID, GOALS},
+        BroadcasterId,
+    },
+    TwitchAPI,
 };
 
-endpoints! {
-    GoalsAPI {
-        /// Gets the broadcaster’s list of active goals
-        ///
-        /// # Arguments
-        ///
-        /// * `broadcaster_id` - The ID of the broadcaster that created the goals.
-        ///
-        /// # Returns
-        ///
-        /// Returns a [`GoalsResponse`]
-        ///
-        /// # Example
-        ///
-        /// ```rust
-        /// # use twitch_highway::TwitchAPI;
-        /// use twitch_highway::{
-        ///     goals::GoalsAPI,
-        ///     types::BroadcasterId
-        /// };
-        ///
-        /// # async fn example(api: TwitchAPI) -> Result<(), Box<dyn std::error::Error>> {
-        /// let response = api
-        ///     .get_creator_goals(&BroadcasterId::from("1234"))
-        ///     .json()
-        ///     .await?;
-        ///
-        /// # Ok(())
-        /// # }
-        /// ```
-        ///
-        /// # Required Scope
-        ///
-        /// `channel:read:goals`
-        ///
-        /// API Reference
-        ///
-        /// <https://dev.twitch.tv/docs/api/reference/#get-creator-goals>
+pub trait GoalsAPI {
+    /// Gets the broadcaster’s list of active goals
+    ///
+    /// # Arguments
+    ///
+    /// * `broadcaster_id` - The ID of the broadcaster that created the goals.
+    ///
+    /// # Returns
+    ///
+    /// Returns a [`GoalsResponse`]
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use twitch_highway::TwitchAPI;
+    /// use twitch_highway::{
+    ///     goals::GoalsAPI,
+    ///     types::BroadcasterId
+    /// };
+    ///
+    /// # async fn example(api: TwitchAPI) -> Result<(), Box<dyn std::error::Error>> {
+    /// let response = api
+    ///     .get_creator_goals(&BroadcasterId::from("1234"))
+    ///     .json()
+    ///     .await?;
+    ///
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// # Required Scope
+    ///
+    /// `channel:read:goals`
+    ///
+    /// API Reference
+    ///
+    /// <https://dev.twitch.tv/docs/api/reference/#get-creator-goals>
+    fn get_creator_goals(&self, broadcaster_id: &BroadcasterId) -> TwitchAPIRequest<GoalsResponse>;
+}
+
+impl GoalsAPI for TwitchAPI {
+    simple_endpoint!(
         fn get_creator_goals(
-            &self,
-            broadcaster_id: &BroadcasterId,
-        ) -> GoalsResponse {
-            endpoint_type: GetCreatorGoals,
+            broadcaster_id: &BroadcasterId [key = BROADCASTER_ID]
+        ) -> GoalsResponse;
+            endpoint: GetCreatorGoals,
             method: GET,
             path: [GOALS],
-            query_params: {
-                query(BROADCASTER_ID, broadcaster_id)
-            }
-        }
-    }
+    );
 }
