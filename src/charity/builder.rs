9382d59b74ac37f4a373b75e245c8014e49685ff
m3id <m3id.eprac@gmail.com>
2025-10-14 11:48:32 +0900

    refactor(charity)!: migrate to builder pattern
    Replace optional parameter-based API with builder pattern.

---
+++ src/charity/builder.rs
@@ -0,0 +1,21 @@
+use crate::{
+    charity::CharityCampaignDonationResponse,
+    types::{
+        constants::{AFTER, BROADCASTER_ID, CHARITY, DONATIONS, FIRST},
+        BroadcasterId,
+    },
+};
+
+define_request_builder! {
+    #[derive(Debug)]
+    GetCharityCampaignDonationBuilder<'a>{
+        req: {broadcaster_id: &'a BroadcasterId [key = BROADCASTER_ID]}
+        opts: {
+            first: u8 [key = FIRST, convert = to_string],
+            after: &'a str [key = AFTER]
+        }
+    } -> CharityCampaignDonationResponse;
+            endpoint_type: GetCharityCampaignDonations,
+            method: GET,
+            path: [CHARITY, DONATIONS],
+}
--- src/charity/mod.rs
+++ src/charity/mod.rs
@@ -1,41 +1,114 @@
+mod builder;
 mod response;
 mod types;
 
+pub use builder::GetCharityCampaignDonationBuilder;
 pub use response::{CharityCampaignDonationResponse, CharityCampaignResponse};
 pub use types::{CharityCampaign, CharityCampaignDonation};
 
-use crate::types::{constants::BROADCASTER_ID, BroadcasterId, PaginationQuery};
+use crate::{
+    request::TwitchAPIRequest,
+    types::{constants::CHARITY, BroadcasterId},
+    TwitchAPI,
+};
 
-const CHARITY: &str = "charity";
+pub trait CharityAPI {
+    /// Gets information about the charity campaign that a broadcaster is running
+    ///
+    /// # Arguments
+    ///
+    /// * `broadcaster_id` -
+    ///
+    /// # Returns
+    ///
+    /// Returns a [`CharityCampaignResponse`]
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use twitch_highway::TwitchAPI;
+    /// use twitch_highway::{
+    ///     charity::CharityAPI,
+    ///     types::BroadcasterId
+    /// };
+    ///
+    /// # async fn example(api: TwitchAPI) -> Result<(), Box<dyn std::error::Error>> {
+    /// let response = api
+    ///     .get_charity_campaign(&BroadcasterId::from("1234"))
+    ///     .json()
+    ///     .await?;
+    ///
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// # Required Scope
+    ///
+    /// `channel:read:charity`
+    ///
+    /// API Reference
+    ///
+    /// <https://dev.twitch.tv/docs/api/reference/#get-charity-campaign>
+    fn get_charity_campaign(
+        &self,
+        broadcaster_id: &BroadcasterId,
+    ) -> TwitchAPIRequest<CharityCampaignResponse>;
 
-endpoints! {
-    CharityAPI {
-        /// <https://dev.twitch.tv/docs/api/reference/#get-charity-campaign>
+    /// Gets the list of donations that users have made to the broadcaster’s active charity campaign
+    ///
+    /// # Arguments
+    ///
+    /// * `broadcaster_id` -
+    ///
+    /// # Returns
+    ///
+    /// Returns a [`GetCharityCampaignDonationBuilder`]
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use twitch_highway::TwitchAPI;
+    /// use twitch_highway::{
+    ///     charity::CharityAPI,
+    ///     types::BroadcasterId
+    /// };
+    ///
+    /// # async fn example(api: TwitchAPI) -> Result<(), Box<dyn std::error::Error>> {
+    /// let response = api
+    ///     .get_charity_campaign_donations(&BroadcasterId::from("1234"))
+    ///     .json()
+    ///     .await?;
+    ///
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// # Required Scope
+    ///
+    /// `channel:read:charity`
+    ///
+    /// API Reference
+    ///
+    /// <https://dev.twitch.tv/docs/api/reference/#get-charity-campaign-donations>
+    fn get_charity_campaign_donations<'a>(
+        &'a self,
+        broadcaster_id: &'a BroadcasterId,
+    ) -> GetCharityCampaignDonationBuilder<'a>;
+}
+
+impl CharityAPI for TwitchAPI {
+    simple_endpoint!(
         fn get_charity_campaign(
-            &self,
             broadcaster_id: &BroadcasterId,
-        ) -> CharityCampaignResponse {
-            endpoint_type: GetCharityCampaign,
+        ) -> CharityCampaignResponse;
+            endpoint: GetCharityCampaign,
             method: GET,
             path: [CHARITY, "campaigns"],
-            query_params: {
-                query(BROADCASTER_ID, broadcaster_id)
-            }
-        }
-
-        /// <https://dev.twitch.tv/docs/api/reference/#get-charity-campaign-donations>
-        fn get_charity_campaign_donations(
-            &self,
-            broadcaster_id: &BroadcasterId,
-            pagination: Option<PaginationQuery>,
-        ) -> CharityCampaignDonationResponse {
-            endpoint_type: GetCharityCampaignDonations,
-            method: GET,
-            path: [CHARITY, "donations"],
-            query_params: {
-                query(BROADCASTER_ID, broadcaster_id),
-                pagination(pagination)
-            }
-        }
+    );
+    fn get_charity_campaign_donations<'a>(
+        &'a self,
+        broadcaster_id: &'a BroadcasterId,
+    ) -> GetCharityCampaignDonationBuilder<'a> {
+        GetCharityCampaignDonationBuilder::new(self, broadcaster_id)
     }
 }
