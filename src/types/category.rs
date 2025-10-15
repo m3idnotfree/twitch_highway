3e053df3a49424356be8902a433e58dc9dac9e4c
m3id <m3id.eprac@gmail.com>
2025-10-16 06:19:21 +0900

    refactor(types): improve Category type safety

--- src/types/category.rs
+++ src/types/category.rs
@@ -1,13 +1,15 @@
 use serde::{Deserialize, Serialize};
+use url::Url;
 
-use crate::types::Id;
+use crate::types::CategoryId;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Category {
-    pub id: Id,
+    /// category or game
+    pub id: CategoryId,
     pub name: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub box_art_url: Option<String>,
+    pub box_art_url: Option<Url>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub igdb_id: Option<String>,
 }
