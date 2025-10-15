a70755a8fedcab2ad9690a82897ab931995eb239
m3id <m3id.eprac@gmail.com>
2025-10-16 05:38:26 +0900

    refactor(types): validate cursor is non-empty on deserialize

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -1,10 +1,20 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 
 /// <https://dev.twitch.tv/docs/api/guide/#pagination>
-/// - after — Use to get the next page of results
-/// - before — Use to get the previous page of results
-/// - first — Use to specify the number of items to include per page
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Pagination {
+    #[serde(deserialize_with = "deserialize_non_empty_string")]
     pub cursor: String,
+}
+
+fn deserialize_non_empty_string<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    if s.is_empty() {
+        Err(serde::de::Error::custom("cursor cannot be empty"))
+    } else {
+        Ok(s)
+    }
 }
